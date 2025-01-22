4b69e597bfca1205c623d1b36d5a67e01e14bdf9
emin.aydin <118691260+eminaydin0@users.noreply.github.com>
2025-01-22 14:14:51 +0300

    page oluşturuldu

--- new/src/app.jsx
+++ new/src/app.jsx
@@ -5,15 +5,7 @@ import './app.css'
 export function App() {
   return (
     <>
-      mustafa
-
-      <div>
-        emin nk sahjs ahsa  dsnhasbhaasx
-        mustafa pull attısss
-      </div>
-      <button>
-        emib
-      </button>
+    
     </>
   )
 }
